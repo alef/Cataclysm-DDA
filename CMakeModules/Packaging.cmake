a56678b1f0d21d02da8882f27cff3618bf6acb82
alef <alef@users.noreply.github.com>
2023-10-05 21:15:35 +0200

    Guess distribution from /etc/os-release

--- CMakeModules/Packaging.cmake
+++ CMakeModules/Packaging.cmake
@@ -1,3 +1,8 @@
+# TODO after CMake v3.22: guess Linux distro via host info command
+if(EXISTS /etc/os-release)
+    file(STRINGS /etc/os-release OS_ID_LIKE REGEX ^ID_LIKE=.+$)
+    string(REGEX REPLACE ^ID_LIKE= "" OS_ID_LIKE ${OS_ID_LIKE})
+endif()
 set(CPACK_GENERATOR ZIP) 
 set(CPACK_PACKAGE_VERSION ${CMAKE_PROJECT_VERSION_MAJOR}.${CMAKE_PROJECT_VERSION_MINOR})
 
